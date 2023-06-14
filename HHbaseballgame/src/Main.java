ac263c15430da308d91de4b205297b36d6f51dfd
junnnnnnnyoung <128367271+junyoung93@users.noreply.github.com>
2023-06-14 10:40:52 +0900

    Update Main.java
    준영 야구코드

--- HHbaseballgame/src/Main.java
+++ HHbaseballgame/src/Main.java
@@ -1,90 +1,106 @@
+package baseball;
+import java.lang.reflect.GenericArrayType;
 import java.util.Scanner;
 
-public class Main {
+//세자리 랜덤숫자 구하기 구현(중복 x)
+// 볼 스트라이크 표현 구현
+// 게임종료하는 부분 구현
+public class RandomNumber {
+
     public static void main(String[] args) {
-        int[] computerNumbers = generateRandomNumbers();
-        int[] userNumbers;
-        int attempts = 0;
+        Scanner scan = new Scanner(System.in);
+        int[] number = new int[3];
+        int[] user = new int[3];
+        int Try = 0;
+        int strike = 0;
+        int ball = 0;
 
         System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
-
-        while (true) {
-            userNumbers = getUserNumbers();
-            attempts++;
-
-            int strikes = countStrikes(computerNumbers, userNumbers);
-            int balls = countBalls(computerNumbers, userNumbers);
-
-            System.out.println(balls+"B"+strikes+"S");
-
-            if (strikes == 3) {
-                System.out.println(attempts+"번만에 맞히셨습니다.");
-                System.out.println("게임을 종료합니다.");
-                break;
-            }
-        }
-    }
-
-    // 0과 9 사이의 서로 다른 3개의 숫자를 무작위로 생성 ==> 해결
-    private static int[] generateRandomNumbers() {
-        int[] numbers = new int[3];
-        boolean[] used = new boolean[10];
-        int count = 0;
-
-        while (count < 3) {  // while보다 for 문으로 짜는 쪽으로...지금은 괜찮지만, 나중을 위해서
-            int randomNumber = (int) (Math.random() * 10);
-//0 1 2 3 4 5 6 7 8 9
-//f f f f f f f T f f      7  //3번만 돌리는게 아니라 count 3이 되기 전까지.
-            if (!used[randomNumber]) {
-                numbers[count] = randomNumber;
-                used[randomNumber] = true;
-                count++;
-            }
-        }
-
-        return numbers;
-    }
-
-    // 사용자로부터 세 자리 숫자를 입력받음  ==> 질문
-    private static int[] getUserNumbers() {
-        Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[3];
-
-        //System.out.print("세 자리 숫자를 입력하세요: ");
-        int input = scanner.nextInt();
-
-        numbers[0] = input / 100;    // 백의 자리 숫자
-        numbers[1] = (input / 10) % 10;    // 십의 자리 숫자
-        numbers[2] = input % 10;    // 일의 자리 숫자
-
-        return numbers;
-    }
-
-    // 스트라이크
-    private static int countStrikes(int[] computerNumbers, int[] userNumbers) {
-        int strikes = 0;
-
+        //랜덤숫자 세자리 뽑기
         for (int i = 0; i < 3; i++) {
-            if (computerNumbers[i] == userNumbers[i]) {
-                strikes++;
-            }
-        }
+            number[i] = (int)(Math.random()*10);
+//            System.out.print(number[i]);
 
-        return strikes;
-    }
-
-    // 볼
-    private static int countBalls(int[] computerNumbers, int[] userNumbers) {
-        int balls = 0;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (i != j && computerNumbers[i] == userNumbers[j]) {
-                    balls++;
+            //중복제거
+            for (int j = 0; j < i; j++) {
+                if (number[i] == number[j]) {
+                    i--;
+                    break;
                 }
             }
         }
 
-        return balls;
+        //유저입력값
+
+        for (int x = 0; x < 10; x++) {
+            Try ++;
+
+//            for (int i = 0; i < 3; i++) {
+////                    user[i] = scan.nextInt();
+//                    user[x] = Integer.parseInt(scan.nextLine());
+////                    temp = user[i];
+//                if(user.length != 3 ){
+//                    System.out.println("3자리 숫자를 입력해주세요.");
+//                }
+////                System.out.println("유저가 입력한 값 : " + user[j] );
+//            }
+            System.out.println(Try+ " 번 째 시도 : ");
+
+            String userInput =  scan.nextLine();
+            if (userInput.length() != 3) {
+                System.out.println("3자리 숫자를 입력해주세요.");
+//                continue;
+            }
+            user[0] = Character.getNumericValue(userInput.charAt(0));   // Character.getNumericValue 는 char 타입의 숫자를 정수값으로 변환
+            user[1] = Character.getNumericValue(userInput.charAt(1));
+            user[2] = Character.getNumericValue(userInput.charAt(2));
+
+            //볼, 스트라이크 구별
+            for (int j = 0; j < 3; j++) {
+
+                for (int k = 0; k < 3; k++) {
+//                    System.out.println("스트라이크");
+                    if (number[j] == user[k]) {
+                        if (j == k) {
+                            strike ++;
+//                            System.out.println("스트라이크");
+                        } else {
+                            ball ++;
+//                            System.out.println("볼");
+                        }
+                    }
+                }
+            }
+            if (strike == 0 && ball == 0) {
+                System.out.println("0B0S");
+            } else if (strike == 1 && ball == 0) {
+                System.out.println("0B1S");
+            } else if (strike == 2 && ball == 0) {
+                System.out.println("0B2S");
+            } else if (strike == 1 && ball == 1) {
+                System.out.println("1B1S");
+            } else if (strike == 1 && ball == 2) {
+                System.out.println("2B1S");
+            } else if (strike == 1 && ball == 3) {
+                System.out.println("3B1S");
+            } else if (strike == 1 && ball == 4) {
+                System.out.println("4B1S");
+            } else if (strike == 0 && ball == 1) {
+                System.out.println("1B0S");
+            } else if (strike == 0 && ball == 2) {
+                System.out.println("2B0S");
+            } else if (strike == 0 && ball == 3) {
+                System.out.println("3B0S");
+            } else if (strike == 0 && ball == 4) {
+                System.out.println("3B0S");
+            } else {
+                System.out.println("3S");
+                System.out.println( Try + "번 만에 맞히셨습니다.");
+                System.out.println("게임을 종료합니다.");
+
+                break;
+            }
+        }
     }
 }
+
